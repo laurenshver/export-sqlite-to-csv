5b9f6e03137463c8cd6a9bbeac56ae72b0a316ee
Lauren S <lauren_shver@hotmail.com>
2021-03-29 20:25:40 -0400

    commit file

---
+++ export.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# argument of database name when being executed
+
+TS=`sqlite3 $1 "SELECT tbl_name FROM sqlite_master WHERE type='table';"`
+
+# exports each table to csv
+for T in $TS; do
+
+sqlite3 $1 <<!
+.headers on
+.mode csv
+.output $T.csv
+select * from $T
+!
+
+done
